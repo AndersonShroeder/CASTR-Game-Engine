b65f7e75e00d888b5139a778ca8358a9c54393be
Tigiy98 <93889399+AndersonShroeder@users.noreply.github.com>
2023-04-19 15:01:22 -0500

    2D world
    Expanded Scene class to support rendering a 2D world based on a grid of numbers.

--- src/scene.cpp
+++ src/scene.cpp
@@ -10,6 +10,15 @@ Scene::Scene()
 
 	glBindVertexArray(VAO);
 }
+Scene::~Scene()
+{
+    glDeleteVertexArrays(1, &VAO);
+	glDeleteProgram(shaderProgram);
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+}
 
 GLFWwindow* Scene::initWindow()
 {
@@ -71,4 +80,176 @@ GLuint Scene::initShader()
 
 	// Return the ID of the compiled and linked shader program
 	return shaderProgram;
+}
+
+
+void Scene::drawGrid()
+{
+	// Specify the color of the lines
+    GLfloat color[] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+    glUseProgram(shaderProgram);
+
+    glBindVertexArray(VAO);
+
+    // Set the line color
+    GLint colorLoc = glGetUniformLocation(shaderProgram, "lineColor");
+    glUniform4fv(colorLoc, 1, color);
+
+    std::vector<GLfloat> vertices;
+    std::vector<GLuint> indices;
+
+    // Add the vertical lines to the vertex buffer
+    for (GLfloat x = -1.0f; x <= 1.0f; x += MAP_STEP_SIZE_HEIGHT)
+    {
+        vertices.push_back(x);
+        vertices.push_back(-1.0f);
+        vertices.push_back(0.0f);
+
+        vertices.push_back(x);
+        vertices.push_back(1.0f);
+        vertices.push_back(0.0f);
+
+        indices.push_back(vertices.size() / 3 - 2);
+        indices.push_back(vertices.size() / 3 - 1);
+    }
+
+    // Add the horizontal lines to the vertex buffer
+    for (GLfloat y = -1.0f; y <= 1.0f; y += MAP_STEP_SIZE_HEIGHT)
+    {
+        vertices.push_back(-1.0f);
+        vertices.push_back(y);
+        vertices.push_back(0.0f);
+
+        vertices.push_back(1.0f);
+        vertices.push_back(y);
+        vertices.push_back(0.0f);
+
+        indices.push_back(vertices.size() / 3 - 2);
+        indices.push_back(vertices.size() / 3 - 1);
+    }
+
+    GLuint gridVBO, gridIBO;
+    glGenBuffers(1, &gridVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, gridVBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+
+    glGenBuffers(1, &gridIBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gridIBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+    glDrawElements(GL_LINES, indices.size(), GL_UNSIGNED_INT, 0);
+
+    glDeleteBuffers(1, &gridVBO);
+    glDeleteBuffers(1, &gridIBO);
+
+    glBindVertexArray(0);
+}
+
+void Scene::drawMap(int map[MAP_HEIGHT*MAP_WIDTH])
+{
+    int startx = -1;
+	int starty = 1;
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    
+    std::vector<GLfloat> vertices;
+    std::vector<GLuint> indices;
+
+    for (int i = 0; i < MAP_HEIGHT*MAP_WIDTH; i++)
+    {
+        GLfloat color[3];
+        if (map[i] == 0)
+        {
+            color[0] = 1.0f;
+            color[1] = 1.0f;
+            color[2] = 1.0f;
+        }
+
+        else
+        {
+            color[0] = 0.0f;
+            color[1] = 0.0f;
+            color[2] = 0.0f;
+        }
+
+        int column = i % 10;
+        int row = i / 10;
+
+        GLfloat top_y = 1 - MAP_STEP_SIZE_HEIGHT * row;
+        GLfloat bottom_y = 1 - MAP_STEP_SIZE_HEIGHT * (row + 1);
+		GLfloat left_x = -1 + MAP_STEP_SIZE_WIDTH * column;
+		GLfloat right_x = -1 + MAP_STEP_SIZE_WIDTH * (column + 1);
+
+        // TOP LEFT VERTEX
+        vertices.push_back(left_x);
+        vertices.push_back(top_y);
+        vertices.push_back(0.0f);
+        vertices.push_back(color[0]);
+        vertices.push_back(color[1]);
+        vertices.push_back(color[2]);
+
+        // TOP RIGHT VERTEX
+        vertices.push_back(right_x);
+        vertices.push_back(top_y);
+        vertices.push_back(0.0f);
+        vertices.push_back(color[0]);
+        vertices.push_back(color[1]);
+        vertices.push_back(color[2]);
+
+        // BOTTOM LEFT VERTEX
+        vertices.push_back(left_x);
+        vertices.push_back(bottom_y);
+        vertices.push_back(0.0f);
+        vertices.push_back(color[0]);
+        vertices.push_back(color[1]);
+        vertices.push_back(color[2]);
+
+        // BOTTOM RIGHT VERTEX
+        vertices.push_back(right_x);
+        vertices.push_back(bottom_y);
+        vertices.push_back(0.0f);
+        vertices.push_back(color[0]);
+        vertices.push_back(color[1]);
+        vertices.push_back(color[2]);
+
+        int n = vertices.size()/6;
+
+        // TRIANGLE 1
+        indices.push_back(n-4);
+        indices.push_back(n-3);
+        indices.push_back(n-2);
+
+        // TRIANGLE 2
+        indices.push_back(n-3);
+        indices.push_back(n-2);
+        indices.push_back(n-1);
+    }
+
+    GLuint gridVBO, gridIBO;
+    glGenBuffers(1, &gridVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, gridVBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+
+    glGenBuffers(1, &gridIBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gridIBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+
+    // Set up vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void *)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+
+    glDeleteBuffers(1, &gridVBO);
+    glDeleteBuffers(1, &gridIBO);
+
+    glBindVertexArray(0);
+
 }
--- src/scene.hh
+++ src/scene.hh
@@ -1,12 +1,15 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <vector>
 #define SCREEN_WDITH 800
 #define SCREEN_HEIGHT 800
 #define MAP_WIDTH 10
 #define MAP_HEIGHT 10
-#define XPIXEL 1/SCREEN_WDITH
-#define YPIXEL 1/SCREEN_HEIGHT
+#define MAP_STEP_SIZE_WIDTH (1.0/MAP_WIDTH) * 2
+#define MAP_STEP_SIZE_HEIGHT (1.0/MAP_HEIGHT) * 2
+#define XPIXEL 2/SCREEN_WDITH
+#define YPIXEL 2/SCREEN_HEIGHT
 
 class Scene
 {
@@ -17,7 +20,13 @@ class Scene
 
     Scene();
 
+    ~Scene();
+
     GLFWwindow* initWindow();
 
     GLuint initShader();
+
+    void drawGrid();
+
+    void drawMap(int map[MAP_HEIGHT*MAP_WIDTH]);
 };
